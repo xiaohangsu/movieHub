b93ba0572c8bde2fe6755e03ea6d8cc0d9e5761a
Xiaohang Su <sxhdragon@gmail.com>
2017-03-27 21:12:22 -0400

    Using agrv to control index.html css and js Dev or Dist

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 require('babel-register');
+
 const Koa   = require('koa');
+const views = require('koa-views');
+
 const app   = new Koa();
 
 const index = require('./routes/index');
 
 app
+    .use(views(__dirname + '/views', {
+        map: {
+            html: 'ejs'
+        }
+    }))
     .use(index.routes())
     .use(index.allowedMethods());
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
+const CONFIG       = require('process').argv[2];
+
 const Router       = require('koa-router');
 const send         = require('koa-send');
 
 let router         = new Router();
+
+let STATIC_PATH = '';
+
+if (CONFIG === 'dev') {
+    STATIC_PATH = 'http://localhost:8080/';
+} else if (CONFIG === 'dist') {
+    STATIC_PATH = '/dist/';
+}
+
+
 router.get('/', (ctx, next)=> {
-    return send(ctx, './index.html');
+    return ctx.render('./index.html', {
+        staticPath: STATIC_PATH
+    });
 });
 
 
