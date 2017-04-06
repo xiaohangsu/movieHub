e12cff5cecdc7230bbc8c1e1cfcb4cd603f1f938
Xiaohang Su <sxhdragon@gmail.com>
2017-04-06 16:15:57 -0400

    index.js entry and index.html fixed

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,18 @@
 const CONFIG       = require('process').argv[2];
-
 const Router       = require('koa-router');
 const send         = require('koa-send');
 
 let router         = new Router();
 
 let STATIC_PATH = '';
-
 if (CONFIG === 'dev') {
     STATIC_PATH = 'http://localhost:8080/';
 } else if (CONFIG === 'dist') {
     STATIC_PATH = '/dist/';
 }
 
-
 router.get('/', (ctx, next)=> {
-    return ctx.render('./index.html', {
+    return ctx.render('../views/index.html', {
         staticPath: STATIC_PATH
     });
 });
