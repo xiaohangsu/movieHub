cb01846491e6182628c86a5b0b0a115491b80201
Xiaohang Su <sxhdragon@gmail.com>
2017-04-06 16:15:30 -0400

    rating Router and Data Model

---
+++ data/rating.js
@@ -0,0 +1,37 @@
+const connection = require('./connection');
+
+// Rating: movid, cusid, rating, timestamp
+
+class Rating {
+    constructor() {
+        this.conn = connection;
+    }
+
+    addRating(json) {
+        return this.conn.query('INSERT INTO Ratings \
+            (movid, cusid, rating) \
+            VALUES (:movid, :cusid, :rating)',
+            {replacements: json});
+    }
+
+    deleteRating(json) {
+        return this.conn.query('DELETE FROM Ratings WHERE movid=:movid and cusid=:cusid',
+            {replacements: json});
+    }
+
+    updateRating(json) {
+        return this.conn.query('UPDATE Ratings\
+            SET rating=:rating WHERE movid=:movid and cusid=:cusid',
+            {replacements: json});
+    }
+
+    groupRatingByMovie(json) {
+        return this.conn.query('SELECT rating, COUNT(rating) as count FROM Ratings \
+            WHERE movid=:movid GROUP BY rating DESC',
+            {replacements: json});
+    }
+}
+
+const rating = new Rating();
+
+module.exports = rating;
---
+++ routes/rating.js
@@ -0,0 +1,88 @@
+const Router    = require('koa-router');
+const rating    = require('./../data/rating');
+
+let router = new Router();
+
+router.post('/db/addRating', async (ctx, next)=> {
+    ctx.body = await rating.addRating(ctx.request.body).then((instance)=> {
+        console.log('/db/addRating POST | Success: ', JSON.stringify(instance[0]));
+        return {
+            status: 200,
+            message: 'Success'
+        };
+    }).catch((err)=> {
+        console.log('/db/addRating POST | ERROR: ', JSON.stringify(err));
+        return {
+            status: 400,
+            error: err
+        };
+    });
+});
+
+router.post('/db/deleteRating', async (ctx, next)=> {
+    ctx.body = await rating.deleteRating(ctx.request.body).then((instance)=> {
+        console.log('/db/deleteRating POST | Success: ', JSON.stringify(instance[0]));
+        if (instance[0].affectedRows != 0) {
+            return {
+                status: 200,
+                message: 'Success'
+            };
+        } else {
+            return {
+                status: 205,
+                message: 'Unmatched'
+            };
+        }
+
+    }).catch((err)=> {
+        console.log('/db/deleteRating POST | ERROR: ', JSON.stringify(err));
+        return {
+            status: 400,
+            error: err
+        };
+    });
+});
+
+router.post('/db/updateRating', async (ctx, next)=> {
+    ctx.body = await rating.updateRating(ctx.request.body).then((instance)=> {
+        console.log('/db/updateRating POST | Success: ', JSON.stringify(instance[0]));
+        if (instance[0].affectedRows != 0 && instance[0].changeRows != 0) {
+            return {
+                status: 200,
+                message: 'Success'
+            };
+        } else {
+            return {
+                status: 205,
+                message: 'Unmatched Or Still the same Rating'
+            };
+        }
+
+    }).catch((err)=> {
+        console.log('/db/updateRating POST | ERROR: ', JSON.stringify(err));
+        return {
+            status: 400,
+            error: err
+        };
+    });
+});
+
+router.post('/db/groupRatingByMovie', async (ctx, next)=> {
+    ctx.body = await rating.groupRatingByMovie(ctx.request.body).then((instance)=> {
+        console.log('/db/groupRatingByMovie POST | Success: ', JSON.stringify(instance[0]));
+        return {
+            status: 200,
+            message: 'Success'
+        };
+    }).catch((err)=> {
+        console.log('/db/groupStarByMovie POST | ERROR: ', JSON.stringify(err));
+        return {
+            status: 400,
+            error: err
+        };
+    });
+});
+
+
+
+module.exports = router;
